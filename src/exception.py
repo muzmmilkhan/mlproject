a71426683ed867c1f18eb88940e89f143f074faf
muzmmil pthan <muzmmil_pathan@apple.com>
2025-07-08 18:50:33 +0530

    Modified exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,13 @@
 import sys
-from logger import logging
 
-def error_message_detail(error, error_detail: sys) -> str:
+def error_message_detail(error: Exception, error_detail: sys) -> str:
     """Generates a detailed error message."""
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     return f"Error occurred in script: {file_name} at line number: {line_number} with message: {str(error)}"
 
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail)
-        
-
-    def __str__(self):
-        return self.error_message
+def CustomException(error: Exception, error_detail: sys) -> Exception:
+    """Custom exception class that includes detailed error information."""
+    error_message = error_message_detail(error, error_detail)
+    return Exception(error_message)
